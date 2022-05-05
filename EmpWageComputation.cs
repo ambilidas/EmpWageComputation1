03ad3c33d6f66fdcf260793f6d8ac9bef543eb65
Ambili das v s <ambili.kan@gmail.com>
2022-05-05 14:26:57 +0530

    Employee wage calculated for a month

--- EmpWageComputation.cs
+++ EmpWageComputation.cs
@@ -12,36 +12,43 @@ namespace EmpWageComputation1
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int WAGE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
 
         int emphr = 0;
         int empwage = 0;
-
+        int totalempwage=0;
         Random random = new Random();
-        
-        
+
+
 
         public void EmpAttendance()
         {
-            int empcheck = random.Next(0, 3);
-         
-            switch(empcheck)
+            
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    emphr = 8;
-                    break;
-                case IS_PART_TIME:
-                    emphr = 4;
-                    break;
-                default: 
-                    emphr = 0;
-                    break;
-            }
-        }
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IS_FULL_TIME:
+                        emphr = 8;
+                        break;
+                    case IS_PART_TIME:
+                        emphr = 4;
+                        break;
+                    default:
+                        emphr = 0;
+                        break;
+                }
 
+                empwage = emphr * WAGE_PER_HOUR;
+                Console.WriteLine("Employee Daily Wage: {0}", empwage);
+                totalempwage += empwage;
+            }
+
+        }
         public void EmpWageCalc()
         {
-            empwage = emphr* WAGE_PER_HOUR;
-            Console.WriteLine("Employee Daily Wage: {0}",empwage);
+            Console.WriteLine("Total employee wage:{0}",totalempwage);
         }
 
 
