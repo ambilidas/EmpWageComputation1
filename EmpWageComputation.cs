1f8050e51170ac8814645eb19a41fba098510b5c
Ambili das v s <ambili.kan@gmail.com>
2022-05-05 13:36:12 +0530

    Part time employee is added

--- EmpWageComputation.cs
+++ EmpWageComputation.cs
@@ -9,20 +9,29 @@ namespace EmpWageComputation1
 
     public class EmpWageComputation
     {
-        int IS_FULL_TIME = 1;
-        Random random = new Random();
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int WAGE_PER_HOUR = 20;
-       // int FULL_DAY_HOUR = 8;
+
         int emphr = 0;
         int empwage = 0;
 
+        Random random = new Random();
+        
+        
+
         public void EmpAttendance()
         {
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             if(empcheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
                 emphr = 8;
+            }
+            else if(empcheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is part time");
+                emphr = 4;
             }
             else
             {
